8883387b16efa27dc3d71ffe22c087f5996ba04d
toshadvidrine <159669056+toshadvidrine@users.noreply.github.com>
2024-04-25 18:22:35 +0000

    created variables

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+   let who = ['My class', 'My kid', 'My sister', 'My boss'];
+let action = ['called', 'text', 'crushed', 'broke'];
+let what = ['my laptop', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
+console.log("Hello Rigo from the console!");
 };
+console.log("Sorry, I can't come");
+;
+let whoIndex=
+Math.floor(Math.random()*
+who.length);
+let actIndex=
+Math.floor(Math.random()*
+action.length);
+let watIndex=
+Math.floor(Math.random()*
+what.length);
+let wenIndex
+Math.floor(Math.random()*
+when.length);
+
--- webpack.config.js
+++ webpack.config.js
@@ -74,8 +74,6 @@ module.exports = {
         filename: "index.html",
         template: "src/index.html"
     }),
-    new PrettierPlugin({
-        failSilently: true
-    }),
+   
   ]
 };
