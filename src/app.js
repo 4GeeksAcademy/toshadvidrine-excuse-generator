36bc9b710bc94da99e582aad1cdbe9a5debe7acc
toshadvidrine <159669056+toshadvidrine@users.noreply.github.com>
2024-04-26 15:38:31 +0000

    rearranged, added window onload and return statement

--- src/app.js
+++ src/app.js
@@ -6,25 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+    document.querySelector("#btn").addEventListener("click", () => {
+        document.querySelector("#the-excuse").innerHTML = genrateExcuse();
+    });
   
-   let who = ['My class', 'My kid', 'My sister', 'My boss'];
-let action = ['called', 'text', 'crushed', 'broke'];
-let what = ['my laptop', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
-console.log("Hello Rigo from the console!");
 };
-console.log("Sorry, I can't come");
-;
-let whoIndex=
-Math.floor(Math.random()*
-who.length);
-let actIndex=
-Math.floor(Math.random()*
-action.length);
-let watIndex=
-Math.floor(Math.random()*
-what.length);
-let wenIndex
-Math.floor(Math.random()*
-when.length);
+
+let genrateExcuse = () => {
+    let who = ['My class', 'My kid', 'My sister', 'My boss'];
+    let action = ['called', 'text', 'crushed', 'broke'];
+    let what = ['my laptop', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
+
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actIndex = Math.floor(Math.random() * action.length);
+    let watIndex = Math.floor(Math.random() * what.length);
+    let wenIndex = Math.floor(Math.random() * when.length);
+
+
+return (
+    who[whoIndex] +
+    " " +
+    action[actIndex] +
+    " " +
+    what[watIndex] +
+    " " +
+    when[wenIndex] 
+);
+
+};
+
+
+
+
+
+
 
